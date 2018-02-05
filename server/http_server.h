b61cb6efc87d0dd18328b92c19b2d71f56253552
cenxinwei <cenxinwei@yy.com>
2018-02-05 06:42:51 -0800

    simple http server

---
+++ server/http_server.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <functional>
+#include <string>
+#include <map>
+#include <libevent/include/event.h>
+#include <libevent/include/evhttp.h>
+
+namespace http {
+
+    class IHandler {
+    public:
+        virtual ~IHandler() {}
+        virtual void handle(struct evhttp_request *req) = 0;
+    };
+
+    class Server {
+    public:
+        Server(){}
+        ~Server(){}
+
+        void Handle(const std::string& uri, IHandler *handler);
+
+        int ListenAndServe(int port, int nthreads, int backlog, IHandler *handler);
+        
+    private:
+        int ensure_listen(int port, int backlog);
+        static void event_cb(struct evhttp_request *req, void *arg);
+        static void* event_dispatch(void *arg);
+    private:
+        std::map<std::string, IHandler*> mux_;
+        IHandler* default_handler_;
+    };
+}
