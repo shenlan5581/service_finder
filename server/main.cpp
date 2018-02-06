7ed14fe9e2f8c9aa024d22b6c6b59a8662659b1d
cenxinwei <cenxinwei@yy.com>
2018-02-06 04:41:31 -0800

    commit miss file

--- server/main.cpp
+++ server/main.cpp
@@ -1,23 +1,42 @@
+#include <stdio.h>
+#include "http_server.h"
+#include <functional>
+#include <string>
+#include <iostream>
+using namespace std;
+using namespace placeholders;
+
+class TestHandler : public http::IHandler {
+public:
+    void handle(struct evhttp_request *req) 
+    {
+        struct evbuffer *buf;
+        buf = evbuffer_new();
+        string resultURL = "thank you\n";
+        cout<<"query end"<<endl;
+        evbuffer_add_printf(buf, resultURL.c_str());
+        evhttp_send_reply(req, HTTP_OK, "OK", buf);
+        cout<<"search end"<<endl;
+        evbuffer_free(buf);    
+    }
+};
 
 
-#include "service_finder_s.h"
-#include "connect.h"
-#include "http_request.h"
-#include "database.h"
+class DefaultHandler : public http::IHandler {
+public:
+    void handle(struct evhttp_request *req) 
+    {
+        struct evbuffer *buf;
+        buf = evbuffer_new();
+        string resultURL = "this is default handler\n";
+        evbuffer_add_printf(buf, resultURL.c_str());
+        evhttp_send_reply(req, HTTP_OK, "OK", buf);
+        evbuffer_free(buf);    
+    }    
+};
 
-k::Connect C;
-k::service_finder_s S;
-
-int main()
-{ 
-   
- k::database sql;
-
- // C.Listen();
-         
-
-
-
-
-
+int main(void) {
+    http::Server server;
+    server.Handle("/test", new TestHandler());
+    server.ListenAndServe(1811, 2, 1024, new DefaultHandler());
 }
